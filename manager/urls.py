73f7e8d494e9b8e511550665247ae128f76efbf9
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-04-27 22:57:43 +0300

    add list of login items

--- manager/urls.py
+++ manager/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 
-from manager.views import main, LoginCreateView,show_secret_buttons
+from manager.views import main, LoginCreateView,show_secret_buttons, LoginListView
 
 app_name = 'manager'
 
 urlpatterns = [
     path('add-login/', LoginCreateView.as_view(), name='add_login'),
+    path('list-logins/', LoginListView.as_view(), name='list_logins'),
     path('secret-buttons/', show_secret_buttons, name='secret_buttons'),
     path('', main, name='main'),
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,12 +1,16 @@
+import logging
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.http import require_GET
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from manager.forms import LoginForm
 from manager.models import Login
+
+logger = logging.getLogger(__name__)
 
 
 @login_required
@@ -31,3 +35,12 @@ class LoginCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+@method_decorator(login_required, name='dispatch')
+class LoginListView(ListView):
+    template_name = 'manager/login_list.html'
+
+    def get_queryset(self):
+        logger.debug("Current user: %s", self.request.user)
+        return Login.objects.filter(user=self.request.user)
